9f4a4e87bffa0b8c55c1adce1094168ca5e8ccba
Marcela <marceabonce221@gmail.com>
2025-02-10 10:16:08 -0500

    squash commit

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -7,4 +7,7 @@ public class ProyectoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProyectoApplication.class, args);
     }
+
+    //Proyecto avanzada
+    //Crando Rama Majo
 }
